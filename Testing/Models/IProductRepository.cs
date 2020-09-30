bcc506b56cb4958c19d18b180b39522d04979a6a
Justus Storm <storm.j.dev@gmail.com>
2020-09-30 14:55:47 -0500

    Completed the webpage, all CRUD methods are implimented and work, could add some functionality in the future to manipulate more data for each product in the database

--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Testing.Models;
 
 namespace Testing
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
+        public IEnumerable<Product> GetAllProducts(); // stubbed out method 
+        public Product GetProduct(int id); // stubbed out method
+
+
+        public void UpdateProduct(Product product); // stubbed out method
+
+
+        public void InsertProduct(Product productToInsert);// stubbed out method
+        public IEnumerable<Category> GetCategories();// stubbed out method
+        public Product AssignCategory();// stubbed out method
+
+        public void DeleteProduct(Product product); // stubbed out method
+
     }
 }
--- Testing/Models/ProductRepository.cs
+++ Testing/Models/ProductRepository.cs
@@ -13,15 +13,59 @@ namespace Testing
         // properties 
         private readonly IDbConnection _conn;
 
+
+
         // Constructors
         public ProductRepository (IDbConnection conn)
         {
             _conn = conn;
         }
 
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+
         public IEnumerable<Product> GetAllProducts()
         {
             return _conn.Query<Product>("SELECT * FROM products;");
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE productid = @id",
+                new { id = id });
+        }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (Name, Price, CategoryID, StockLevel) VALUES (@name, @price, @categoryID, @stockLevel);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID, stockLevel = productToInsert.StockLevel});
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name , Price = @price WHERE ProductID = @id;",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM reviews WHERE ProductID = @id;",
+                new { id = product.ProductID });
+            _conn.Execute("DELETE FROM sales WHERE ProductID = @id;",
+                new { id = product.ProductID });
+            _conn.Execute("DELETE FROM products WHERE ProductID = @id;",
+                new { id = product.ProductID });
         }
     }
 }
