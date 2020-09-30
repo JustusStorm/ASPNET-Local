561b7ab6de7f2a25c0ae5eff5c2cd1159295a262
Justus Storm <storm.j.dev@gmail.com>
2020-09-30 10:36:14 -0500

    Pushing project to save data, and to have a copy on github

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        // properties 
+        private readonly IDbConnection _conn;
+
+        // Constructors
+        public ProductRepository (IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+    }
+}
